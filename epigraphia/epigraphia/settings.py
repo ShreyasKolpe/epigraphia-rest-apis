7e4e8ff0090e2ccf2fbb04724fabf1dc7e9183ce
Shreyas Kolpe <shreyas10.kolpe@gmail.com>
2022-05-26 12:55:01 +0530

    Update settings.py

--- epigraphia/epigraphia/settings.py
+++ epigraphia/epigraphia/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ya&97d*kv%*5bf$ul*%k$n9qd4jwem=42e2+#rjo0&0^8v5&c0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
