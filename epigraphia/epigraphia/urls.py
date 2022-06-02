07a4a1f21dae1dd171ee0e140052f72e4aea5806
Shreyas Kolpe <shreyas10.kolpe@gmail.com>
2022-06-02 09:06:44 +0530

    Cleaner url and link on admin page

--- epigraphia/epigraphia/urls.py
+++ epigraphia/epigraphia/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apis.urls')),
+    path('cleaner/', include('cleaner.urls')),
     path('', include('ui.urls'))
 ]
