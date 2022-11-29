4292218002466b7b72ecc653d8c6809c592af1fd
Shreyas Kolpe <shreyas10.kolpe@gmail.com>
2022-11-29 10:12:18 +0530

    Separated CSS, JS for indic-text-input into separate files, updated indic text input with fraction input

--- epigraphia/cleaner/forms.py
+++ epigraphia/cleaner/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class CleanerForm(forms.Form):
-    input_str = forms.CharField(widget=forms.Textarea, label='Paste here the unclean text')
+    input_str = forms.CharField(widget=forms.Textarea, label='Paste here the text to be cleaned')
     lang = forms.ChoiceField(choices=[('kn', 'Kannada')], label='Choose language of the script')
     process_e = forms.BooleanField(label='Choose whether to find and substitute ē', required=False)
     process_o = forms.BooleanField(label='Choose whether to find and substitute ō', required=False)
     process_s = forms.BooleanField(label='Choose whether to find and substitute the common sequence śrī', required=False)
-    process_n = forms.BooleanField(label='Choose whether to smartly process anusvara-consonant clusters', required=False)
+    process_n = forms.BooleanField(label='Choose whether to smartly process anunasika-consonant clusters', required=False)
